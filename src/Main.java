e89647fc11f1273935a48eb336a27087634f5d27
Laureen Tellez Zamora <151061783+laureenTellezz@users.noreply.github.com>
2024-02-27 14:03:40 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ import director.ClazadoBuilderDirector;
 import Class.*;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         Calzado calzado;
         ClazadoBuilderDirector clazadoBuilderDirector = new ClazadoBuilderDirector();
         CalzadoBuilder calzadoBuilder = new CalzadoBuilder();
         clazadoBuilderDirector.buildCalzadoCordonAmarillo(calzadoBuilder);
         calzado = calzadoBuilder.getCalsado();
-        System.out.println("Calzado: "+ calzado.getColor());
+        System.out.println("Calzado: "+ calzado.getColor()+" "+"Tipo"+" "+calzado.getTipoCordon());
 
     }
 }
