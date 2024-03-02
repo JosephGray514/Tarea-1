56179540cc3d953f3bf9b84e64d2f4fe260b9963
Laureen Tellez Zamora <151061783+laureenTellezz@users.noreply.github.com>
2024-03-01 22:58:19 -0600

    Add files via upload

--- src/gestor/Gestor.java
+++ src/gestor/Gestor.java
@@ -4,78 +4,78 @@ import director.ClazadoBuilderDirector;
 import Class.*;
 import ticket.Boletin;
 
-import java.io.BufferedReader;
-import java.io.*;
 public class Gestor {
-    public static void CalsadoAmarillo(){
-        // Se declara el calzado
+
+    public static String CalzadoConCordon(){
         Calzado calzado;
-        // Se declara el Director
+
         ClazadoBuilderDirector calzadoBuilderDirector = new ClazadoBuilderDirector();
-        // Se declara el Builder
+
         CalzadoBuilder calzadoBuilder = new CalzadoBuilder();
-        //El metodo del calzado deseado del Director recibe por parametro el Builder
-        calzadoBuilderDirector.buildCalzadoCordonAmarillo(calzadoBuilder);
-        //El calzado toma el valor del calzado creado por el Builder
+
+        calzadoBuilderDirector.buildCalzadoCordon(calzadoBuilder);
+
         calzado = calzadoBuilder.getCalsado();
 
-//     Negro
-//\u001B[30m
-//      Rojo
-//\u001B[31m
-//     Verde
-//\u001B[32m
-//   Amarillo
-//\u001B[33m
-//      Azul
-//\u001B[34m
-//   Púrpura
-//\u001B[35m
-//      Cian
-//\u001B[36m
-//    Blanco
-//\u001B[37m
+        Boletin boletin = new Boletin();
+        String mensaje = boletin.boletinCalzadoCordon(calzado);
 
+        return mensaje;
     }
-    public static void CalsadoNegro(){
-        // Se declara el calzado
-        Calzado calzado;
-        // Se declara el Director
-        ClazadoBuilderDirector calzadoBuilderDirector = new ClazadoBuilderDirector();
-        // Se declara el Builder
-        CalzadoBuilder calzadoBuilder = new CalzadoBuilder();
-        //El metodo del calzado deseado del Director recibe por parametro el Builder
-        calzadoBuilderDirector.buildCalzadoCordonNegro(calzadoBuilder);
-        //El calzado toma el valor del calzado creado por el Builder
-        calzado = calzadoBuilder.getCalsado();
 
-    }
-    public static void CalsadoVelcroAzul (){
-        // Se declara el calzado
+    public static String CalzadoConVelcro(){
+
         Calzado calzado;
-        // Se declara el Director
+
         ClazadoBuilderDirector calzadoBuilderDirector = new ClazadoBuilderDirector();
-        // Se declara el Builder
+
         CalzadoBuilder calzadoBuilder = new CalzadoBuilder();
-        //El metodo del calzado deseado del Director recibe por parametro el Builder
+
         calzadoBuilderDirector.buildCalzadoVelcro(calzadoBuilder);
-        //El calzado toma el valor del calzado creado por el Builder
+
         calzado = calzadoBuilder.getCalsado();
 
+        Boletin boletin = new Boletin();
+
+        String mensaje = boletin.boletinCalzadoVelcro(calzado);
+
+        return mensaje;
     }
-    public static void CalzadoTacon  (){
+
+    public static String CalzadoConAlmohadilla(){
+
         Calzado calzado;
-        // Se declara el Director
+
         ClazadoBuilderDirector calzadoBuilderDirector = new ClazadoBuilderDirector();
-        // Se declara el Builder
+
         CalzadoBuilder calzadoBuilder = new CalzadoBuilder();
-        //El metodo del calzado deseado del Director recibe por parametro el Builder
-        calzadoBuilderDirector.buildCalzadoJoseph(calzadoBuilder);
-        //El calzado toma el valor del calzado creado por el Builder
+
+        calzadoBuilderDirector.buildCalzadoConAlmohadilla(calzadoBuilder);
+
+        calzado = calzadoBuilder.getCalsado();
+
+        Boletin boletin = new Boletin();
+
+        String mensaje = boletin.boletinCalzadoConAlmohadilla(calzado);
+
+        return mensaje;
+    }
+
+    public static String CalzadoConTacon  (){
+        Calzado calzado;
+
+        ClazadoBuilderDirector calzadoBuilderDirector = new ClazadoBuilderDirector();
+
+        CalzadoBuilder calzadoBuilder = new CalzadoBuilder();
+
+        calzadoBuilderDirector.buildCalzadoTacon(calzadoBuilder);
+
         calzado = calzadoBuilder.getCalsado();
         Boletin boletin = new Boletin();
-        
 
+        String mensaje = boletin.boletinCalzadoTacon(calzado);
+
+        return mensaje;
     }
 
 
